c9ca7af44a98eb3c35eed0cff1ca0f15fa175813
elintonev@hotmail.com <elintonev@hotmail.com>
2022-05-15 21:41:51 -0300

    Inicializing the table increment

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let AccessToken = async() => {
     const retorno = await fetch(`https://autenticador.secullum.com.br/Token?${grant}&username=${userName}&password=${password}&client_id=${client}`, { method: "POST" }).then((response) => response.json())
 
     token = `${retorno.access_token}`
-    accessToken.innerHTML = token
+    accessToken.value = token
 }
 
 let BaseList = async() => {
@@ -65,7 +65,7 @@ let ReqLiberated = () => {
 
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
-            alert('Banco Selecionado com sucesso')
+            console.log('Banco selecionado com sucesso')
         }
     }
     xhr.send();
@@ -74,6 +74,51 @@ let ReqLiberated = () => {
 let AllRegisterFiltered = async() => {
     let firstDate = document.getElementById('firstDate').value
     let lastDate = document.getElementById('lastDate').value
+    let table = document.createElement('table')
+    let thead = document.createElement('thead')
+    let tbody = document.createElement('tbody')
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    document.getElementById('body').appendChild(table)
+
+    let row_1 = document.createElement('tr');
+    let heading_1 = document.createElement('th');
+    heading_1.innerHTML = "Data";
+    let heading_2 = document.createElement('th');
+    heading_2.innerHTML = "Entrada 1";
+    let heading_3 = document.createElement('th');
+    heading_3.innerHTML = "Saída 1";
+    let heading_4 = document.createElement('th');
+    heading_4.innerHTML = "Entrada 2";
+    let heading_5 = document.createElement('th');
+    heading_5.innerHTML = "Saída 2";
+    let heading_6 = document.createElement('th');
+    heading_6.innerHTML = "Entrada 3";
+    let heading_7 = document.createElement('th');
+    heading_7.innerHTML = "Saída 3";
+    let heading_8 = document.createElement('th');
+    heading_8.innerHTML = "Entrada 4";
+    let heading_9 = document.createElement('th');
+    heading_9.innerHTML = "Saída 4";
+    let heading_10 = document.createElement('th');
+    heading_10.innerHTML = "Entrada 5";
+    let heading_11 = document.createElement('th');
+    heading_11.innerHTML = "Saída 5";
+
+    row_1.appendChild(heading_1);
+    row_1.appendChild(heading_2);
+    row_1.appendChild(heading_3);
+    row_1.appendChild(heading_4);
+    row_1.appendChild(heading_5);
+    row_1.appendChild(heading_6);
+    row_1.appendChild(heading_7);
+    row_1.appendChild(heading_8);
+    row_1.appendChild(heading_9);
+    row_1.appendChild(heading_10);
+    row_1.appendChild(heading_11);
+    thead.appendChild(row_1);
+
 
     let url = `https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna/Batidas?dataInicio=${firstDate}&dataFim=${lastDate}`
 
@@ -85,7 +130,23 @@ let AllRegisterFiltered = async() => {
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             let teste = JSON.parse(xhr.responseText)
-            console.log(teste)
+            let aux = teste.map(list => {
+                const newList = {
+                    data: list.Data,
+                    entrada1: list.Entrada1,
+                    saida1: list.Saida1,
+                    entrada2: list.Entrada2,
+                    saida2: list.Saida2,
+                    entrada3: list.Entrada3,
+                    saida3: list.Saida3,
+                    entrada4: list.Entrada4,
+                    saida4: list.Saida4,
+                    entrada5: list.Entrada5,
+                    saida5: list.Saida5,
+                    nfolha: list.Funcionario.NumeroFolha
+                }
+                return newList;
+            })
         }
     }
     xhr.send();
