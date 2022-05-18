567e304a6b56c25df99687fb559e40617dde77e3
elintonev@hotmail.com <elintonev@hotmail.com>
2022-05-17 23:10:47 -0300

    Integracao de batidas finalizada

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let button = document.getElementById('ButtonSoliciteToken')
 let baselist = document.getElementById('ButtonSoliciteBaseList')
 let selectDataBase = document.getElementById('BaseList')
 let registerList = document.getElementById('registerList')
+let radio1 = document.getElementById('gerarCompleto')
+let radio2 = document.getElementById('gerarResumido')
+let paragraph = document.getElementById('hiddenParagraph')
 var token = null
 
 let AccessToken = async() => {
@@ -74,8 +77,8 @@ let ReqLiberated = () => {
 let AllRegisterFiltered = async() => {
     let firstDate = document.getElementById('firstDate').value
     let lastDate = document.getElementById('lastDate').value
-    let tabela = document.getElementById('tableList')
-	let fileName = document.getElementById('FileName').value
+    let fileName = document.getElementById('FileName').value
+
 
     let url = `https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna/Batidas?dataInicio=${firstDate}&dataFim=${lastDate}`
 
@@ -103,45 +106,59 @@ let AllRegisterFiltered = async() => {
                     nfolha: list.Funcionario.NumeroFolha
                 }
                 return newList;
-            }).filter( testeFilter => {
-				return testeFilter.entrada1 !== null || testeFilter.entrada2 !== null || testeFilter.entrada3 !== null || testeFilter.entrada4 !== null || testeFilter.entrada5 !== null || 
-				testeFilter.saida1 !== null || testeFilter.saida2 !== null || testeFilter.saida3 !== null || testeFilter.saida4 !== null || testeFilter.saida5 !== null
-			} )
-			console.log(arq(aux))
-			
-			
-			
-			
-            downloadFiles(arq(aux), `${fileName}`, 'txt')
+            }).filter(testeFilter => {
+                return testeFilter.entrada1 !== null || testeFilter.entrada2 !== null || testeFilter.entrada3 !== null || testeFilter.entrada4 !== null || testeFilter.entrada5 !== null ||
+                    testeFilter.saida1 !== null || testeFilter.saida2 !== null || testeFilter.saida3 !== null || testeFilter.saida4 !== null || testeFilter.saida5 !== null
+            })
+            radio1.checked ? console.log(teste) : downloadFiles(arq(aux), `${fileName}`, 'txt')
+
         }
     }
     xhr.send();
 }
 
-let arq = (arquivo) =>{
-	let result = ''
-	for (let obj of arquivo){		
-		result += `${obj.data} ---> ${obj.entrada1}  ${obj.saida1} ---> ${obj.nfolha}`
-		result += `\n`
-	}
-	return result
+let arq = (arquivo) => {
+    let result = ''
+    var date;
+    for (let obj of arquivo) {
+        date = new Date(obj.data)
+        dia = date.getDate()
+        mes = date.getMonth() + 1
+        if (dia < 10) {
+            dia = '0' + dia
+        }
+        if (mes < 10) {
+            mes = '0' + mes
+        }
+        result += `${dia}/${mes}/${date.getFullYear()}    --->     Entrada 1: ${obj.entrada1}    Saída 1: ${obj.saida1}    Entrada 2: ${obj.entrada2}    Saída 2: ${obj.saida2}    --->     N° Folha: ${obj.nfolha}`
+        result += `\n`
+    }
+    return result
 }
 
 function downloadFiles(data, file_name, file_type) {
-    var file = new Blob([data], {type: file_type});
-    if (window.navigator.msSaveOrOpenBlob){ 
-	window.navigator.msSaveOrOpenBlob(file, file_name);}
-    else { 
+    var file = new Blob([data], { type: file_type });
+    if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(file, file_name);
+    } else {
         var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
+            url = URL.createObjectURL(file);
         a.href = url;
         a.download = file_name;
         document.body.appendChild(a);
         a.click();
         setTimeout(function() {
             document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);  
-        }, 0); 
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+
+let showParagraph = (p) => {
+    if (radio2.checked) {
+        p.setAttribute('style', 'display: none;')
+    } else {
+        p.setAttribute('style', 'display: block;')
     }
 }
 
